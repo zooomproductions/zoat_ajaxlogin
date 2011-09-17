4896f5755d0ef0bf80517380013c339f84f32c51
Arno Schoon <arno@maxserv.nl>
2011-09-17 21:21:01 +0000

    Added snippet to use t3sec_satedpw (http://typo3.org/documentation/document-library/extension-manuals/t3sec_saltedpw/0.2.13/view/1/4/)
    git-svn-id: http://svn.t3o.typo3.org/extensions/projects/t3org/ajaxlogin/trunk@1522 e8cb78a8-64f2-4011-8042-bd1ea5058d47

--- Classes/Utility/Password.php
+++ Classes/Utility/Password.php
@@ -13,7 +13,14 @@ class Tx_Ajaxlogin_Utility_Password {
 				$saltingInstance = tx_saltedpasswords_salts_factory::getSaltingInstance();
 				$string = $saltingInstance->getHashedPassword($string);
 			}
-		}
+		} else if (t3lib_extMgm::isLoaded('t3sec_saltedpw')) { 
+			require_once t3lib_extMgm::extPath('t3sec_saltedpw').'res/staticlib/class.tx_t3secsaltedpw_div.php'; 
+			if (tx_t3secsaltedpw_div::isUsageEnabled()) { 
+				require_once t3lib_extMgm::extPath('t3sec_saltedpw').'res/lib/class.tx_t3secsaltedpw_phpass.php'; 
+				$objPHPass = t3lib_div::makeInstance('tx_t3secsaltedpw_phpass'); 
+				$string = $objPHPass->getHashedPassword($string); 
+			} 
+		} 
 			
 		return $string;
 	}
