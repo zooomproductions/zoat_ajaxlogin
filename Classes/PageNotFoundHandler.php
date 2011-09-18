05c9f68c42f1ba2ce63c7aec02847b887944175f
Arno Schoon <arno@maxserv.nl>
2011-09-18 10:26:34 +0000

    For some reason realurl issues a 401 if there's a problem with the pagepath, added a second check to PageNotFoundHandler.php
    git-svn-id: http://svn.t3o.typo3.org/extensions/projects/t3org/ajaxlogin/trunk@1531 e8cb78a8-64f2-4011-8042-bd1ea5058d47

--- Classes/PageNotFoundHandler.php
+++ Classes/PageNotFoundHandler.php
@@ -3,9 +3,9 @@
 class Tx_Ajaxlogin_PageNotFoundHandler {
 	
 	public function handleError($params, tslib_fe $pObj) {
-		if(isset($params['pageAccessFailureReasons']['fe_group']) && !isset($params['pageAccessFailureReasons']['hidden'])) {
-			//header(t3lib_utility_Http::HTTP_STATUS_401, true);
-			
+		
+		if(isset($params['pageAccessFailureReasons']['fe_group']) && !isset($params['pageAccessFailureReasons']['hidden'])
+			&& current($params['pageAccessFailureReasons']['fe_group']) !== 0) { // make sure realurl does't issue this 401			
 			$code = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ajaxlogin']['unauthorized_handling'];
 			$header = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ajaxlogin']['unauthorized_handling_statheader'];
 			
