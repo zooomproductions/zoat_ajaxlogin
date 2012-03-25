6af206e2d34a6ecb66ed14e8a56a9db0f83f586c
Tolleiv Nietsch <info@tolleiv.de>
2012-03-25 18:32:18 +0000

    [BUGFIX] Filter input of resetAction.
    git-svn-id: http://svn.t3o.typo3.org/extensions/projects/t3org/ajaxlogin/trunk@2048 e8cb78a8-64f2-4011-8042-bd1ea5058d47

--- Classes/Controller/UserController.php
+++ Classes/Controller/UserController.php
@@ -1,16 +1,16 @@
 <?php
 class Tx_Ajaxlogin_Controller_UserController extends Tx_Extbase_MVC_Controller_ActionController {
-	
+
 	/**
 	 * @var Tx_Ajaxlogin_Domain_Repository_UserRepository
 	 */
-	protected $userRepository;	
-	
+	protected $userRepository;
+
 	/**
 	 * @var Tx_Ajaxlogin_Domain_Repository_UserGroupRepository
 	 */
-	protected $userGroupRepository;	
-	
+	protected $userGroupRepository;
+
 	public function initializeAction() {
 		$this->userRepository = t3lib_div::makeInstance('Tx_Ajaxlogin_Domain_Repository_UserRepository');
 		$this->userGroupRepository = t3lib_div::makeInstance('Tx_Ajaxlogin_Domain_Repository_UserGroupRepository');
@@ -27,27 +27,27 @@ class Tx_Ajaxlogin_Controller_UserController extends Tx_Extbase_MVC_Controller_A
 	protected function getErrorFlashMessage() {
 		return false;
 	}
-	
-	public function infoAction() {		
+
+	public function infoAction() {
 		$user = $this->userRepository->findCurrent();
-		
+
 		if(!is_null($user)) {
-			$this->view->assign('user', $user);			
+			$this->view->assign('user', $user);
 		} else {
 			$this->response->setStatus(401);
 			$this->forward('login');
 		}
 	}
-	
+
 	public function loginAction() {
 		$token = 'tx-ajaxlogin-form' . time();
 		$this->view->assign('formToken', $token);
 		$this->response->setHeader('X-Ajaxlogin-formToken', $token);
 	}
-	
+
 	public function authenticateAction() {
 		$user = $this->userRepository->findCurrent();
-		
+
 		if (!is_null($user)) {
 			$message = Tx_Extbase_Utility_Localization::translate('login_successful', 'ajaxlogin');
 			$this->flashMessageContainer->add($message, '', t3lib_FlashMessage::OK);
@@ -66,118 +66,118 @@ class Tx_Ajaxlogin_Controller_UserController extends Tx_Extbase_MVC_Controller_A
 			$this->forward('login');
 		}
 	}
-	
+
 	/**
 	 * Displays a form for creating a new blog
-     *
-     * @param Tx_Ajaxlogin_Domain_Model_User $newUser A fresh user object taken as a basis for the rendering
-     * @return void
-     * @dontvalidate $user
+	 *
+	 * @param Tx_Ajaxlogin_Domain_Model_User $newUser A fresh user object taken as a basis for the rendering
+	 * @return void
+	 * @dontvalidate $user
 	 */
-	public function newAction(Tx_Ajaxlogin_Domain_Model_User $user = null) {		
+	public function newAction(Tx_Ajaxlogin_Domain_Model_User $user = null) {
 		if (!is_null($user)) {
 			$this->response->setStatus(409);
 		}
-		
+
 		$token = 'tx-ajaxlogin-form' . time();
 		$this->view->assign('formToken', $token);
 		$this->response->setHeader('X-Ajaxlogin-formToken', $token);
-		
+
 		$this->view->assign('user', $user);
 	}
-	
+
 	/**
 	 * Creates a new user
-     *
-     * @param Tx_Ajaxlogin_Domain_Model_User $user A fresh User object which has not yet been added to the repository
-     * @param string $password_check
-     * @return void
+	 *
+	 * @param Tx_Ajaxlogin_Domain_Model_User $user A fresh User object which has not yet been added to the repository
+	 * @param string $password_check
+	 * @return void
 	 */
 	public function createAction(Tx_Ajaxlogin_Domain_Model_User $user, $password_check) {
-		$objectError = t3lib_div::makeInstance('Tx_Extbase_Validation_PropertyError', 'user');			
-		$emailError = t3lib_div::makeInstance('Tx_Extbase_Validation_PropertyError', 'email');			
-		$usernameError = t3lib_div::makeInstance('Tx_Extbase_Validation_PropertyError', 'username');			
+		$objectError = t3lib_div::makeInstance('Tx_Extbase_Validation_PropertyError', 'user');
+		$emailError = t3lib_div::makeInstance('Tx_Extbase_Validation_PropertyError', 'email');
+		$usernameError = t3lib_div::makeInstance('Tx_Extbase_Validation_PropertyError', 'username');
 		$passwordError = t3lib_div::makeInstance('Tx_Extbase_Validation_PropertyError', 'password');
-			
+
 		$checkEmail = $this->userRepository->findOneByEmail($user->getEmail());
 		$checkUsername = $this->userRepository->findOneByUsername($user->getUsername());
-		
+
 		if (!is_null($checkEmail)) {
 			$emailError->addErrors(array(
 				t3lib_div::makeInstance('Tx_Extbase_Error_Error', 'Duplicate email address', 1320783534)
 			));
 		}
-		
+
 		if (!is_null($checkUsername)) {
 			$usernameError->addErrors(array(
 				t3lib_div::makeInstance('Tx_Extbase_Error_Error', 'Duplicate username', 1320703758)
 			));
 		}
-		
-		if(strcmp($user->getPassword(), $password_check) != 0) {			
+
+		if(strcmp($user->getPassword(), $password_check) != 0) {
 			$passwordError->addErrors(array(
 				t3lib_div::makeInstance('Tx_Extbase_Error_Error', 'Password does not match', 1320703779)
 			));
 		}
-		
+
 		if(count($emailError->getErrors())) {
 			$objectError->addErrors(array(
 				$emailError
 			));
 		}
-		
+
 		if(count($usernameError->getErrors())) {
 			$objectError->addErrors(array(
 				$usernameError
 			));
 		}
-		
+
 		if(count($passwordError->getErrors())) {
 			$objectError->addErrors(array(
 				$passwordError
 			));
 		}
-		
+
 		if(count($objectError->getErrors())) {
 			$requestErrors = $this->request->getErrors();
-			
+
 			$requestErrors[] = $objectError;
-			
+
 			$this->request->setErrors($requestErrors);
 			$this->forward('new', null, null, $this->request->getArguments());
 		}
-		
+
 		$userGroups = $this->userGroupRepository->findByUidArray(t3lib_div::intExplode(',', $this->settings['defaultUserGroups']));
-		
+
 		$password = $user->getPassword();
-		
+
 		$password = Tx_Ajaxlogin_Utility_Password::salt($password);
-		
+
 		foreach ($userGroups as $userGroup) {
 			$user->getUsergroup()->attach($userGroup);
 		}
 
 		$user->setPassword($password);
-		
+
 		// add a hash to verify the account by sending an e-mail
 		$user->setVerificationHash(md5(t3lib_div::generateRandomBytes(64)));
-		
+
 		$this->userRepository->add($user);
 		$this->userRepository->_persistAll();
-		
+
 		Tx_Ajaxlogin_Utility_FrontendUser::signin($user);
 
 		$message = Tx_Extbase_Utility_Localization::translate('signup_successful', 'ajaxlogin');
 		$this->flashMessageContainer->add($message, '', t3lib_FlashMessage::OK);
-		
+
 		$this->view->assign('user', $user);
-		
+
 		$emailSubject = Tx_Extbase_Utility_Localization::translate('signup_notification_subject', 'ajaxlogin', array(
 				t3lib_div::getIndpEnv('TYPO3_HOST_ONLY')
 		));
-		
+
 		$emailBodyContent = $this->view->render();
-		
+
 		$mail = t3lib_div::makeInstance('t3lib_mail_Message');
 		$mail->setFrom(array($this->settings['notificationMail']['emailAddress'] => $this->settings['notificationMail']['sender']));
 		$mail->setTo(array($user->getEmail() => $user->getName()));
@@ -191,10 +191,10 @@ class Tx_Ajaxlogin_Controller_UserController extends Tx_Extbase_MVC_Controller_A
 		if (!empty($redirect_url)) {
 			$this->response->setHeader('X-Ajaxlogin-redirectUrl', $redirect_url);
 		}
-		
+
 		$this->forward('info');
 	}
-	
+
 	public function logoutAction() {
 		$message = Tx_Extbase_Utility_Localization::translate('logout_successful', 'ajaxlogin');
 		$this->flashMessageContainer->add($message, '', t3lib_FlashMessage::NOTICE);
@@ -202,55 +202,55 @@ class Tx_Ajaxlogin_Controller_UserController extends Tx_Extbase_MVC_Controller_A
 		$GLOBALS['TSFE']->fe_user->logoff();
 		$this->forward('login');
 	}
-	
+
 	public function showAction() {
 		$user = $this->userRepository->findCurrent();
-		
+
 		$this->view->assign('user', $user);
 	}
-	
+
 	public function editAction() {
 		$this->view->assign('user', $this->userRepository->findCurrent());
 	}
-	
+
 	/**
 	 * Updates an existing user
-	 * 
+	 *
 	 * @param Tx_Ajaxlogin_Domain_Model_User
 	 * @return void
 	 */
 	public function updateAction(Tx_Ajaxlogin_Domain_Model_User $user) {
 		$objectError = t3lib_div::makeInstance('Tx_Extbase_Validation_PropertyError', 'user');
 		$emailError = t3lib_div::makeInstance('Tx_Extbase_Validation_PropertyError', 'email');
-			
+
 		$checkEmail = $this->userRepository->findOneByEmail($user->getEmail());
-		
+
 		if (!is_null($checkEmail) && $checkEmail->getUid() != $user->getUid()) {
 			$emailError->addErrors(array(
 				t3lib_div::makeInstance('Tx_Extbase_Error_Error', 'Duplicate email address', 1320783534)
 			));
 		}
-		
+
 		if(count($emailError->getErrors())) {
 			$objectError->addErrors(array(
 				$emailError
 			));
 		}
-		
+
 		if(count($objectError->getErrors())) {
 			$requestErrors = $this->request->getErrors();
-			
+
 			$requestErrors[] = $objectError;
-			
+
 			$this->request->setErrors($requestErrors);
 			$this->forward('edit', null, null, $this->request->getArguments());
 		}
-		
+
 		$this->userRepository->update($user);
 		$this->flashMessageContainer->add('User updated');
 		$this->redirect('show');
 	}
-	
+
 	/**
 	 * @param string $verificationHash
 	 * @param string $email
@@ -259,64 +259,65 @@ class Tx_Ajaxlogin_Controller_UserController extends Tx_Extbase_MVC_Controller_A
 		if(!empty($verificationHash) && !empty($email)) {
 			$user = $this->userRepository->findOneByVerificationHashAndEmail($verificationHash, $email);
 		}
-		
+
 		if(!is_null($user)) {
 			$userGroups = $this->userGroupRepository->findByUidArray(t3lib_div::intExplode(',', $this->settings['defaultUserGroupsAfterVerification']));
-		
+
 			foreach ($userGroups as $userGroup) {
 				$user->getUsergroup()->attach($userGroup);
 			}
-			
+
 			$user->setVerificationHash(null);
-	
+
 			$this->userRepository->update($user);
 			$this->userRepository->_persistAll();
-			
+
 			Tx_Ajaxlogin_Utility_FrontendUser::signin($user);
 			$this->redirectToURI('/');
 		} else {
 			$this->response->setStatus(409);
 		}
 	}
-	
+
 	public function forgotPasswordAction() {
 		$token = 'tx-ajaxlogin-form' . time();
 		$this->view->assign('formToken', $token);
 		$this->response->setHeader('X-Ajaxlogin-formToken', $token);
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * Enter description here ...
 	 * @param string $usernameOrEmail
 	 */
 	public function resetPasswordAction($usernameOrEmail = '') {
 		$user = null;
-		
+		$usernameOrEmail = filter_var($usernameOrEmail, FILTER_SANITIZE_SPECIAL_CHARS);
+
 		if(!empty($usernameOrEmail) && t3lib_div::validEmail($usernameOrEmail)) {
 			$user = $this->userRepository->findOneByEmail($usernameOrEmail);
 		} else if(!empty($usernameOrEmail)) {
 			$user = $this->userRepository->findOneByUsername($usernameOrEmail);
 		}
-		
+
 		if(!is_null($user)) {
 			$user->setForgotHash(md5(t3lib_div::generateRandomBytes(64)));
 			$user->setForgotHashValid((time() + (24 * 3600)));
 			$this->view->assign('user', $user);
-			
+
 			$emailSubject = Tx_Extbase_Utility_Localization::translate('resetpassword_notification_subject', 'ajaxlogin', array(
 				t3lib_div::getIndpEnv('TYPO3_HOST_ONLY')
 			));
-		
+
 			$emailBodyContent = $this->view->render();
-		
+
 			$mail = t3lib_div::makeInstance('t3lib_mail_Message');
 			$mail->setFrom(array($this->settings['notificationMail']['emailAddress'] => $this->settings['notificationMail']['sender']));
 			$mail->setTo(array($user->getEmail() => $user->getName()));
 			$mail->setSubject($emailSubject);
 			$mail->setBody($emailBodyContent);
 			$mail->send();
-			
+
 			$message = Tx_Extbase_Utility_Localization::translate('resetpassword_notification_sent', 'ajaxlogin');
 			$this->flashMessageContainer->add($message, '', t3lib_FlashMessage::OK);
 
@@ -328,7 +329,7 @@ class Tx_Ajaxlogin_Controller_UserController extends Tx_Extbase_MVC_Controller_A
 			$this->forward('forgotPassword');
 		}
 	}
-	
+
 	/**
 	 * @param string $forgotHash
 	 * @param string $email
@@ -339,14 +340,14 @@ class Tx_Ajaxlogin_Controller_UserController extends Tx_Extbase_MVC_Controller_A
 		} else {
 			$user = $this->userRepository->findCurrent();
 		}
-		
+
 		if(!is_null($user)) {
 			$this->view->assign('user', $user);
 		} else {
 			$this->response->setStatus(401);
 		}
 	}
-	
+
 	public function closeAccountAction() {
 		$this->view->assign('user', $this->userRepository->findCurrent());
 	}
@@ -370,43 +371,43 @@ class Tx_Ajaxlogin_Controller_UserController extends Tx_Extbase_MVC_Controller_A
 		$objectError = t3lib_div::makeInstance('Tx_Extbase_Validation_PropertyError', 'user');
 		$passwordError = t3lib_div::makeInstance('Tx_Extbase_Validation_PropertyError', 'password');
 		$passwordValidator = t3lib_div::makeInstance('Tx_Ajaxlogin_Domain_Validator_CustomRegularExpressionValidator');
-		
+
 		$passwordValidator->setOptions(array(
 			'object' => 'User',
 			'property' => 'password'
 		));
-		
+
 		if (empty($password['new'])) {
 			$passwordError->addErrors(array(
 				t3lib_div::makeInstance('Tx_Extbase_Error_Error', 'Password can not be blank', 1320792347)
 			));
 		}
-		
+
 		if (strcmp($password['new'], $password['check']) != 0) {
 			$passwordError->addErrors(array(
 				t3lib_div::makeInstance('Tx_Extbase_Error_Error', 'Password does not match', 1320703779)
 			));
 		}
-		
+
 		if (!$passwordValidator->isValid($password['new'])) {
 			$passwordError->addErrors($passwordValidator->getErrors());
 		}
-		
+
 		if(count($passwordError->getErrors())) {
 			$objectError->addErrors(array(
 				$passwordError
 			));
 		}
-		
+
 		if(count($objectError->getErrors())) {
 			$requestErrors = $this->request->getErrors();
-			
+
 			$requestErrors[] = $objectError;
-			
+
 			$this->request->setErrors($requestErrors);
 			$this->forward('editPassword', null, null, $this->request->getArguments());
 		}
-		
+
 		$saltedPW = Tx_Ajaxlogin_Utility_Password::salt($password['new']);
 		$user->setPassword($saltedPW);
 		$user->setForgotHash('');
