0061909270eb0631ebe1f9950959cc96626a8cd8
Tolleiv Nietsch <info@tolleiv.de>
2012-03-25 19:27:18 +0000

    [BUGFIX] Make sure we stay on the same host when we redirect after the login
    git-svn-id: http://svn.t3o.typo3.org/extensions/projects/t3org/ajaxlogin/trunk@2049 e8cb78a8-64f2-4011-8042-bd1ea5058d47

--- Classes/Utility/RedirectUrl.php
+++ Classes/Utility/RedirectUrl.php
@@ -1,14 +1,38 @@
 <?php
 
 class Tx_Ajaxlogin_Utility_RedirectUrl {
-	
+
+	/**
+	 * @static
+	 * @param $url
+	 * @return mixed
+	 * @throws InvalidArgumentException
+	 */
+	protected static function sanitizeUrl($url) {
+
+			// finds the main domain of the current host (www.typo3.org => typo3.org)
+		$serverName = preg_filter('/.*(\.[\w\-_]+\.[\w]+)$/', '$1', t3lib_div::getIndpEnv('HTTP_HOST'));
+		$parts = parse_url($url);
+
+		if (!preg_match('/' .$serverName. '$/', $parts['host'])) {
+			throw new InvalidArgumentException('Url is supposed to belong to ');
+		}
+		return $url;
+	}
+
+	/**
+	 * @static
+	 * @param $url
+	 * @param string $fallback
+	 * @return mixed
+	 */
 	public static function findRedirectUrl($url, $fallback = '') {
 		$res = '';
-			
+
 		if(!empty($fallback)) {
 			$res = $fallback;
 		}
-		
+
 		if(!empty($url)) {
 			$parts = parse_url($url);
 			
@@ -31,8 +55,8 @@ class Tx_Ajaxlogin_Utility_RedirectUrl {
 				t3lib_div::callUserFunction($_funcRef, $_params, $this);
 			}
 		}
-		
-		return $res;
+
+		return self::sanitizeUrl($res);
 	}
 
 }
