1d1ad80eb0477908e9fb1c06f06b544a825b146c
Tolleiv Nietsch <info@tolleiv.de>
2012-03-26 14:02:17 +0000

    [BUGFIX] Add the servername to the error-message
    git-svn-id: http://svn.t3o.typo3.org/extensions/projects/t3org/ajaxlogin/trunk@2054 e8cb78a8-64f2-4011-8042-bd1ea5058d47

--- Classes/Utility/RedirectUrl.php
+++ Classes/Utility/RedirectUrl.php
@@ -11,11 +11,11 @@ class Tx_Ajaxlogin_Utility_RedirectUrl {
 	protected static function sanitizeUrl($url) {
 
 			// finds the main domain of the current host (www.typo3.org => typo3.org)
-		$serverName = preg_filter('/.*(\.[\w\-_]+\.[\w]+)$/', '$1', t3lib_div::getIndpEnv('HTTP_HOST'));
+		$serverName = preg_replace('/.*(\.[\w\-_]+\.[\w]+)$/', '$1', t3lib_div::getIndpEnv('HTTP_HOST'));
 		$parts = parse_url($url);
 
 		if (!preg_match('/' .$serverName. '$/', $parts['host'])) {
-			throw new InvalidArgumentException('Url is supposed to belong to ');
+			throw new InvalidArgumentException('Url is supposed to belong to ' . $serverName);
 		}
 		return $url;
 	}
